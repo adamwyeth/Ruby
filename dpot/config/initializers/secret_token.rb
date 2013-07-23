31b5638bf182e1b03837319ba10aa69853c9a3e4
adamwyeth <adamwyeth1@gmail.com>
2013-07-23 13:50:11 -0500

    First Ruby Project

---
+++ dpot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dpot::Application.config.secret_key_base = 'e03e8cce7fbf19a413cc2dc7ef67f245a999c9cc82c0f753e25e31334acb9b9f4aeb3b9cbe2978638b9d4bbb6cfc3e42c9f165b9bae691493fa1b1966243af67'
